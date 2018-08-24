b35f0aa53af59bb53a5e7c424c4bd4a5e88215d5
Lucineide Almeida <lvcalmeida@gmail.com>
2018-08-24 15:08:40 -0300

    Issue 4

--- resources/views/disciplinas/index.blade.php
+++ resources/views/disciplinas/index.blade.php
@@ -1,3 +1,4 @@
+<a href="/disciplinas/create">CADASTRAR DISCIPLINA</a>
 <ul>
 @foreach ($disciplinas as $disciplina)
 	<li><a href="/disciplinas/{{$disciplina->id}}">{{ $disciplina->titulo }}</a></li>
